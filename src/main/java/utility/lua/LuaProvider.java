fa6462b7d894f073d34a28b534cb4aae769ff782
Rob Duff <rdduff64@gmail.com>
2019-01-19 19:36:10 -0600

    Defined missing unpack function.

--- src/main/java/utility/lua/LuaProvider.java
+++ src/main/java/utility/lua/LuaProvider.java
@@ -18,13 +18,23 @@ public class LuaProvider
 	public static Globals lua ( String key )
 	{
 		if ( !luaMap.containsKey ( key ) )
-			luaMap.put ( key, JsePlatform.debugGlobals() );
+			luaMap.put ( key, makeGlobals() );
 		
 		return luaMap.get ( key );
 	}
 	
 	public static Globals newLua()
 	{
-	    return JsePlatform.debugGlobals ();
+	    return makeGlobals();
+	}
+	
+	private static Globals makeGlobals()
+	{
+	    Globals globals = JsePlatform.debugGlobals ();
+	    
+	    // Create the Missing unpack() function
+	    globals.load ( "function unpack (t,i) i=i or 1 if t[i]~=nil then return t[i],unpack(t,i+1) end end" ).call();
+	    
+	    return globals;
 	}
 }
---
+++ src/test/java/utility/lua/LuaProviderTest.java
@@ -0,0 +1,16 @@
+package utility.lua;
+
+import org.junit.Test;
+import org.luaj.vm2.Globals;
+
+public class LuaProviderTest
+{
+    @Test
+    public void testUnpack()
+    {
+        Globals lua = LuaProvider.newLua ();
+        
+        lua.load ( "function testUnpack() local x = { 1, 2, 3 } a,b,c = unpack(x) print ( a, b, c ) end" ).call ();
+        lua.load ( "testUnpack()" ).call ();
+    }
+}
